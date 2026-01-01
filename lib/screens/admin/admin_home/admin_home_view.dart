2f9dff2ab30ea0754379654a5bcd864215ad99c3
RubaZyada <robazyada167@gmail.com>
2026-01-01 21:15:22 +0200

    admin home screen

---
+++ lib/screens/admin/admin_home/admin_home_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../constants/app_colors.dart';
+import 'admin_home_controller.dart';
+import 'widgets/admin_home_top_section.dart';
+import 'widgets/quick_actions_section.dart';
+import 'widgets/top_recyclers_section.dart';
+
+class AdminHomeView extends GetView<AdminHomeController> {
+  const AdminHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const AdminHomeTopSection(),
+
+              Padding(
+                padding: EdgeInsets.all(16.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    QuickActionsSection(),
+                    SizedBox(height: 16),
+                    TopRecyclersSection(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
